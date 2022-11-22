daafe95aac4cb6fc182fb9989c0273d42bfeb659
Leandro <leandrocavalcantifilho@gmail.com>
2022-11-22 19:51:53 -0300

    add scripts

---
+++ Db_Script/Create_Users.sql
@@ -0,0 +1,17 @@
+create database db_base;
+use db_base;
+
+create table tb_users
+(
+	id int primary key auto_increment,
+    nome varchar(200),
+    email varchar(200) unique,
+    senha varchar(200)
+);
+
+insert into tb_users(nome, email, senha) values
+("Nome", "email@email.com", "Senha");
+
+drop table tb_pessoa;
+
+SELECT * FROM db_base.tb_users;
